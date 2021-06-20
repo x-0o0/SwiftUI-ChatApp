a36efda147af07a9147421cd47c7c2cb5e46c4ac
Jaesung <jaesung.lee@sendbird.com>
2021-06-20 12:23:17 +0900

    Initial Commit

---
+++ ChatSwiftUI/ChatSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatSwiftUIApp.swift
+//  ChatSwiftUI
+//
+//  Created by Jaesung Lee on 2021/06/20.
+//
+
+import SwiftUI
+
+@main
+struct ChatSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
